413bfe33a7c61e8073b67b121385d549dbc3f26a
eclpsr <eclpsr@users.noreply.github.com>
2018-02-12 14:01:52 +1000

    111

--- mail.php
+++ mail.php
@@ -1,11 +1,18 @@
 ﻿<?php
-$to = "balupwns@gmail.com"; // емайл получателя данных из формы 
-$tema = "Форма обратной связи на PHP"; // тема полученного емайла 
-$message = "Ваше имя: ".$_POST['name']."<br>";//присвоить переменной значение, полученное из формы name=name
-  $message .= "E-mail: ".$_POST['email']."<br>"; //полученное из формы name=email
-$message .= "Номер телефона: ".$_POST['phone']."<br>"; //полученное из формы name=phone
-$message .= "Сообщение: ".$_POST['message']."<br>"; //полученное из формы name=message
-$headers  = 'MIME-Version: 1.0' . "\r\n"; // заголовок соответствует формату плюс символ перевода строки
-  $headers .= 'Content-type: text/html; charset=utf-8' . "\r\n"; // указывает на тип посылаемого контента
-mail($to, $tema, $message, $headers); //отправляет получателю на емайл значения переменных
+if(isset($_POST["submit"])) {
+$recipient = "balupwns@gmail.com"; //my email
+echo $subject = 'Email message from Point Plumbing';
+echo $name = $_POST ["yourName"];
+echo $email = $_POST["yourEmail"];
+echo $phone = $_POST["yourPhone"];
+echo $location = $_POST["yourLocate"];
+echo  $message = $_POST["yourMessage"];
+
+ $mailBody="Name: $name\nEmail: $email\n\n$message"; 
+
+ mail($recipient, $subject, $mailBody, "From: $name <$email>");
+
+echo $thankYou="<p>Thank you! We will be in contact with you shortly.</p>";
+
+}
 ?>
